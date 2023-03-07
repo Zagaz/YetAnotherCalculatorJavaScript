f9b34443ec2828be6a7de4663c2cf7a1e712e81c
André Ranulfo <30916115+Zagaz@users.noreply.github.com>
2023-03-07 15:31:17 -0300

    Update main.js
    Beautify code

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-
-var arrayNum1        = []; //Primeiro parametro
-var arrayNum2        = []; //Segundo parametro
-var arrayTest        = [];
-var arrayNumeros     = [];
+var arrayNum1 = []; //Primeiro parametro
+var arrayNum2 = []; //Segundo parametro
+var arrayTest = [];
+var arrayNumeros = [];
 var arrayNumerosTemp = [];
-var ans         ;
+var ans;
 var lastNumber;
 var isEqual = false;
 
@@ -17,134 +16,126 @@ document.body.onload = intro();
 // Veja as referencias em add1().
 
 function add(value) {
-    if (arrayNumerosTemp.length <13) {
+    if (arrayNumerosTemp.length < 13) {
         isEqual = false;
         num = document.getElementById(value).innerText;
         addArray(num);
-        disableKey('iEqual','auto');
+        disableKey('iEqual', 'auto');
     }
-   
+
 }
 
 //PASSO 2 -----------------------------------------
 //a função addArray() recebe o parametro das função do PASSO 1.
 
-function addArray(n/*Parametro recebido no PASSO 1*/) {
-//Adiciona o valoe de n no array1  
-popZero() 
-arrayNumeros.push(n);
-arrayNumerosTemp.push(n);
-addTela(arrayNumerosTemp.join(""))
+function addArray(n /*Parametro recebido no PASSO 1*/ ) {
+    //Adiciona o valoe de n no array1  
+    popZero()
+    arrayNumeros.push(n);
+    arrayNumerosTemp.push(n);
+    addTela(arrayNumerosTemp.join(""))
 }
 
 // INTRO () is called once on load.
 function intro() {
     limpar(); //Resets the calculator. 
-    addTela("Hello World!"); 
+    addTela("Hello World!");
     console.log(lastNumber)
 }
-   
+
 //RESET   
 function limpar() {
     addTela(0);
     isEqual = false;
     arrayNum1 = [];
     arrayNum2 = [];
-    operador  = "";
+    operador = "";
     zeraArrayNum();
-    disableKey('iEqual','none');
-    disableKey('iDot','auto');
+    disableKey('iEqual', 'none');
+    disableKey('iDot', 'auto');
     arrayNumeros.push('(');
-    ans ="";
-}
-    
-function computar(c){
-    
-    
-if(!isEqual){
-    addTela(arrayNumerosTemp.join("")); 
-    arrayNumeros.push(c);
-    arrayNumerosTemp = []; 
-    disableKey('iDot','auto');
-    isEqual = false;
-
-
-}
-if(isEqual){
-    arrayNumeros.push(ans);
-    arrayNumeros.push(c);
-    disableKey('iDot','auto');
-    isEqual = false;
+    ans = "";
 }
 
-          
+function computar(c) {
 
 
+    if (!isEqual) {
+        addTela(arrayNumerosTemp.join(""));
+        arrayNumeros.push(c);
+        arrayNumerosTemp = [];
+        disableKey('iDot', 'auto');
+        isEqual = false;
 
 
+    }
+    if (isEqual) {
+        arrayNumeros.push(ans);
+        arrayNumeros.push(c);
+        disableKey('iDot', 'auto');
+        isEqual = false;
+    }
+  
+}
 
-       
-       
-        } 
 
-
-function  point(p) {
+function point(p) {
     var point = p;
-    disableKey('iDot','none');
+    disableKey('iDot', 'none');
 }
-     
-function raiz(){
+
+function raiz() {
     let temp;
     arrayNumeros.shift();
 
-    if(ans == ""){
-     temp = eval (arrayNumeros.join(""));
+    if (ans == "") {
+        temp = eval(arrayNumeros.join(""));
     }
 
-    if(ans != ""){
+    if (ans != "") {
         temp = ans;
 
     }
-   
+
     //let temp = arrayNumeros.join(""); 
-   let raiz = Math.sqrt(temp);
-   addTela(raiz);
-   // zeraArrayNum();
-  //  disableKey('iEqual','auto');
+    let raiz = Math.sqrt(temp);
+    addTela(raiz);
+    // zeraArrayNum();
+    //  disableKey('iEqual','auto');
 }
-   
-function pctgem(){ 
+
+function pctgem() {
     arrayNumeros.shift();
     let temp;
-    if (ans == ""){
+    if (ans == "") {
 
-        temp = eval (arrayNumeros.join("")); 
-       
+        temp = eval(arrayNumeros.join(""));
+
     }
-    if (ans != ""){
+    if (ans != "") {
         temp = ans;
     }
-    
-    let pct = temp/100;
+
+    let pct = temp / 100;
     addTela(pct);
     zeraArrayNum();
-    disableKey('iEqual','auto');
-}    
-       
+    disableKey('iEqual', 'auto');
+}
+
 function equal(key) {
     arrayNumeros.push(')')
     let numero = arrayNumeros.join("");
-    let operacao = eval (numero);
+    let operacao = eval(numero);
     console.log(operacao);
     addTela(operacao);
-    arrayNumeros = []; 
+    arrayNumeros = [];
     ans = operacao;
     arrayNumerosTemp = [];
     arrayNumeros.push('(')
-    
+
     isEqual = true;
-    
-    disableKey(key,'none');
+
+    disableKey(key, 'none');
 }
 
 //Printa no InneHTML da tela o valor.
@@ -153,34 +144,34 @@ function addTela(n) {
     var telaTemp = n;
     let tela = document.getElementById("iOutput");
     tela.value = telaTemp;
-    
+
 }
 
 //Evita que o primeiro valor da parcela seja / ou * .
 function popZero() {
-    if(arrayNumeros[0] === "/" || arrayNumeros[0]==="*" ){
-        arrayNumeros[0]="";
+    if (arrayNumeros[0] === "/" || arrayNumeros[0] === "*") {
+        arrayNumeros[0] = "";
         addTela(0);
     }
 }
 
 function zeraArrayNum() {
-    arrayNumeros = [];   
-    arrayNumerosTemp = [];  
+    arrayNumeros = [];
+    arrayNumerosTemp = [];
 }
-    
-function disableKey(key,status) {
+
+function disableKey(key, status) {
     document.getElementById(key).style.pointerEvents = status;
 }
 
-function mousePointer(id){
-    
-    var pointer = document.getElementById(id); 
+function mousePointer(id) {
+
+    var pointer = document.getElementById(id);
     pointer.style.cursor = "Pointer";
 }
 
-function mouseDefault(id){
+function mouseDefault(id) {
 
-    var pointer = document.getElementById(id); 
+    var pointer = document.getElementById(id);
     pointer.style.cursor = "default";
 }
